bd7d624d837491bfbbef144b9ae0b48b9659b386
Seth Brown <wsethbrown@gmail.com>
2021-09-30 16:18:14 -0400

    Add Navbar Component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 60px;
+    background-color: black;
+`
+
+const Home = () => {
+    return(
+        <Container>
+            <Navbar/>
+        </Container>
+    )
+}
+
+export default Home
