d2eebb01b3e9d3bef34846068e959934b848c7b2
Seth Brown <wsethbrown@gmail.com>
2021-10-01 13:05:38 -0400

    Add styling to Product.jsx, fix typo in Home.jsx by import Product -> Products

--- src/data.js
+++ src/data.js
@@ -65,10 +65,10 @@ export const popularProducts = [
       id:6,
       img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
     },
-    {
-      id:7,
-      img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
-    },
+    // {
+    //   id:7,
+    //   img:"https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    // },
     {
       id:8,
       img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
-import Products from '../components/Product'
+import Products from '../components/Products'
 
 
 const Home = () => {
