6fa8ce4955093a2bf9723957b0a75c32d1bc8933
Seth Brown <wsethbrown@gmail.com>
2021-10-04 11:52:27 -0400

    finish product, register, login pages. Add cart page.

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,105 @@
+import styled from 'styled-components'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import { Button } from '@material-ui/core'
+
+
+const Container = styled.div``
+
+const Wrapper = styled.div`
+    padding: 20px;
+`
+
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+    border: ${props=>props.type === "filled" && "none"};
+    background-color: ${props=>props.type === "filled" ? "black" : "transparent"};
+    color: ${props=>props.type === "filled" && "white"}
+`
+
+const TopTexts = styled.div``
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+const Info = styled.div`
+    flex: 3;
+`
+const Product = styled.div`
+    display: flex;
+    justify-content:L space-between
+`
+const ProductDetail = styled.div``
+const Image = styled.img``
+const Details = styled.div``
+const ProductName = styled.span``
+const ProductId = styled.span``
+const ProductColor = styled.div``
+const ProductSize = styled.span``
+
+const PriceDetail = styled.div``
+
+const Summary = styled.div`
+    flex: 1;
+`
+
+const Cart = () => {
+    return(
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <Title>YOUR BAG</Title>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopTexts>
+                        <TopText>Shopping Bag (2)</TopText>
+                        <TopText>Your Wishlist (0)</TopText>
+                    </TopTexts>
+                    <TopButton type="filled">CHECKOUT NOW</TopButton>
+                </Top>
+                <Bottom>
+                    <Info>
+                        <Product>
+                            <ProductDetail>
+                                <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A"/>
+                                <Details>
+                                    <ProductName><b>Product:</b> JESSIE THUNDER SHOES</ProductName>
+                                    <ProductId><b>ID:</b> 9381248759</ProductId>
+                                    <ProductColor />
+                                    <ProductSize><b>Size:</b> 11.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>price</PriceDetail>
+                        </Product>
+                    </Info>
+                    <Summary>summary</Summary>
+                </Bottom>
+            </Wrapper>
+            <Footer />
+        </Container>
+    )
+}
+
+export default Cart
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,31 +3,37 @@ import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
-import { Description } from '@material-ui/icons'
+import { Description, Add, Remove } from '@material-ui/icons'
 
 const Container = styled.div``
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
 `
+
 const ImageContainer = styled.div`
     flex: 1;
 `
+
 const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
 `
+
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
 `
+
 const Title = styled.h1`
     font-weight: 200;
 `
+
 const Desc = styled.p`
     margin: 20px 0px;
 `
+
 const Price = styled.span`
     font-weight: 100;
     font-size: 40px;
@@ -58,8 +64,44 @@ const FilterColor = styled.div`
     margin: 0px 5px;
     cursor: pointer;
 `
-const FilterSize = styled.select``
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`
 const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+const Amount = styled.div`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`
+const Button = styled.div`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover {
+        background-color: #f8f4f4;
+    }
+`
 
 const Product = () => {
     return(
@@ -92,6 +134,14 @@ const Product = () => {
                             </FilterSize>
                         </Filter>
                     </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove/>
+                            <Amount>1</Amount>
+                            <Add/>
+                        </AmountContainer>
+                        <Button>ADD TO CART</Button>
+                    </AddContainer>
                 </InfoContainer>
             </Wrapper>
             <Newsletter />
