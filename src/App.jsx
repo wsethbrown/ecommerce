d25bdd38afe84205d7be9e65560d8b1c7b332acf
Seth Brown <wsethbrown@gmail.com>
2021-10-04 10:09:57 -0400

    add color options to Product.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+
+import Product from './pages/Product'
 import Home from './pages/Home'
+import ProductList from './pages/ProductList'
 
 const App = () => {
-  return <Home/>;
+  return <Product/>;
 };
 
 export default App;
